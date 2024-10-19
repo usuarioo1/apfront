3aa475c67c6ad8e3f343b579cd2e8d323054db5d
unknown <nicolasgonzalezmoller@gmail.com>
2024-10-18 21:28:39 -0300

    fd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,12 +22,13 @@ const Navbar = () => {
                 alt='logo'
                 width={400}
                 height={100} />
-                
             </div></Link>
+
             {/* Muestra "Artesanías Pachy" solo en pantallas medianas o más grandes */}
             <div className="hidden md:flex flex-1 text-center">
-                
             </div>
+
+            {/* Menú hamburguesa para pantallas pequeñas */}
             <div className="flex-none md:hidden">
                 <button onClick={toggleMenu} className="btn btn-ghost">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -35,6 +36,8 @@ const Navbar = () => {
                     </svg>
                 </button>
             </div>
+
+            {/* Menú en pantallas grandes */}
             <div className="hidden md:flex flex-none">
                 <ul className="menu menu-horizontal px-1">
                     <li className='text-xl'><Link href="/">Inicio</Link></li>
@@ -57,7 +60,7 @@ const Navbar = () => {
                 </ul>
             </div>
 
-            {/* Carrito de compras */}
+            {/* Carrito de compras en pantallas grandes */}
             <div className="hidden md:flex items-center">
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -90,17 +93,41 @@ const Navbar = () => {
                             <details>
                                 <summary>Productos</summary>
                                 <ul className="bg-slate-100 rounded-md p-2 z-50 w-auto">
-                                <li><Link href="/anillos">Anillos</Link></li>
-                                <li><Link href="/colgantes">Colgantes</Link></li>
-                                <li><Link href="/pulseras">Pulseras</Link></li>
-                                <li><Link href="/conjuntos">Conjuntos</Link></li>
-                                <li><Link href="/collares">Collares</Link></li>
-                                <li><Link href="/aros">Aros</Link></li>
-                                <li><Link href="/figuras">Figuras</Link></li>
+                                    <li><Link href="/anillos">Anillos</Link></li>
+                                    <li><Link href="/colgantes">Colgantes</Link></li>
+                                    <li><Link href="/pulseras">Pulseras</Link></li>
+                                    <li><Link href="/conjuntos">Conjuntos</Link></li>
+                                    <li><Link href="/collares">Collares</Link></li>
+                                    <li><Link href="/aros">Aros</Link></li>
+                                    <li><Link href="/figuras">Figuras</Link></li>
                                 </ul>
                             </details>
                         </li>
                         <li><Link href="#">Contacto</Link></li>
+
+                        {/* Carrito de compras en pantallas pequeñas */}
+                        <li>
+                            <div className="dropdown dropdown-start">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                                    <div className="indicator">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                        </svg>
+                                        <span className="badge badge-sm indicator-item">{cantidadTotal}</span>
+                                    </div>
+                                </div>
+                                <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-blue-700 shadow z-50">
+                                    <div className="card-body">
+                                        <span className="font-bold text-lg">{cantidadTotal} Productos</span>
+                                        <div className="card-actions">
+                                            <Link href='/cart'>
+                                                <button className="btn btn-primary btn-block">Ver Carrito</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             )}
