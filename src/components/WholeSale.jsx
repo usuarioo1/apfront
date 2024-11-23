9f8639265157e650817264cf78c9a3d1f3ce80c4
unknown <nicolasgonzalezmoller@gmail.com>
2024-11-23 01:23:03 -0300

    franja en rojo

--- src/components/WholeSale.jsx
+++ src/components/WholeSale.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const FranjaInformativa = () => {
     return (
         <div className="bg-white py-4 shadow-md">
-            <p className="text-center text-gray-800 font-semibold">
+            <p className="text-center text-red-600 font-semibold text-3xl">
                 <span className="block">
-                    Los precios por mayor serán aplicados en compras superiores a <span className="text-blue-600">$100.000</span>.
+                    Los precios por mayor serán aplicados en compras superiores a <span className="text-blue-600">$100.000</span>
                 </span>
                 <span className="block mt-2">
                     ¡Y en compras superiores a <span className="text-green-600">$150.000</span> el envío es <span className="text-red-600 font-bold">GRATIS</span>!
