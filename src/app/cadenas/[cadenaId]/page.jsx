c8a7677a163163440b7fb235d7b5379adcae0ed8
usuario01 <nicolasgonzalezmoller@gmail.com>
2025-12-10 16:28:23 -0300

    precio cadenas unitario

--- src/app/cadenas/[cadenaId]/page.jsx
+++ src/app/cadenas/[cadenaId]/page.jsx
@@ -68,7 +68,7 @@ const DetallesCadenas = ({params}) => {
                     <p className="text-gray-600 mt-2"> stock :{cadena.stock}</p>
                     <hr className="border-gray-300 my-2 w-full" />
                     <div className="flex items-center mt-2">
-                        <p className="text-gray-900 font-bold text-xl mr-4">Precio: ${cadena.precio}</p>
+                        <p className="text-gray-900 font-bold text-xl mr-4">Precio: ${Math.round(cadena.precio)}</p>
                         <button onClick={handleAddToCart} className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-6 rounded-full transition-colors duration-300">Agregar al carrito</button>
                     </div>  
                     <hr className="border-gray-300 my-2 w-full" />
